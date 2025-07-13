aa29b4bf330c06bb662856fea70e25846b5d873e
Prasath V S <87992758+vsp1601@users.noreply.github.com>
2025-07-13 12:25:17 +0530

    API Gateway Done

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,4 +1,30 @@
 package com.pm.analyticsservice.kafka;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
 public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event){
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            // Business Logic Here: Call Service Layer, Repository, DB Related
+
+            log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
+                    patientEvent.getPatientId(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail());
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error while parsing message {}", e.getMessage());
+        }
+    }
 }
